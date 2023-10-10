29c5e35bb7c4edd7027454b3a8035879bd10c053
Jan Nouza <xnouza@fi.muni.cz>
2023-10-10 14:13:29 +0200

    Init project

---
+++ src/main/java/cz/muni/fi/pv168/project/ui/MainWindow.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pv168.project.ui;
+
+import javax.swing.*;
+
+public class MainWindow {
+
+    private final JFrame frame = new JFrame();
+    public void show() {
+        frame.setVisible(true);
+    }
+}
