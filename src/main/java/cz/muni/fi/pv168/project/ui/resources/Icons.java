f4c56fc5e32e27220dd2ba7100e5696034eb9606
Adam Paulen <xpaulen@fi.muni.cz>
2023-11-01 22:09:04 +0100

    new icons

--- src/main/java/cz/muni/fi/pv168/project/ui/resources/Icons.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/resources/Icons.java
@@ -4,10 +4,10 @@ import javax.swing.*;
 import java.net.URL;
 
 public final class Icons {
-    public static final Icon ADD_ICON = createIcon("/Crystal_Clear_action_edit_add.png");
-    public static final Icon EDIT_ICON = createIcon("/Crystal_Clear_action_edit.png");
-    public static final Icon DELETE_ICON = createIcon("/Crystal_Clear_action_button_cancel.png");
-    public static final Icon EXIT = createIcon("/Crystal_Clear_action_exit.png");
+    public static final Icon ADD_ICON = createIcon("/add.png");
+    public static final Icon EDIT_ICON = createIcon("/edit.png");
+    public static final Icon DELETE_ICON = createIcon("/delete.png");
+    public static final Icon EXIT = createIcon("/exit.png");
 
     private Icons() {
         throw new AssertionError("This class is not instantiable");
