eef98ea31d442f9d3ff1f7a2b76bd2b359d59930
Jan Nouza <xnouza@fi.muni.cz>
2023-11-04 12:17:17 +0100

    Refactor TemplateRenderer to use Template class

--- src/main/java/cz/muni/fi/pv168/project/ui/renderers/TemplateRenderer.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/renderers/TemplateRenderer.java
@@ -1,16 +1,16 @@
 package cz.muni.fi.pv168.project.ui.renderers;
 
-import cz.muni.fi.pv168.project.model.Ride;
+import cz.muni.fi.pv168.project.model.Template;
 
 import javax.swing.*;
 
-public class TemplateRenderer extends AbstractRenderer<Ride> {
+public class TemplateRenderer extends AbstractRenderer<Template> {
 
     public TemplateRenderer(){
-        super(Ride.class);
+        super(Template.class);
     }
     @Override
-    protected void updateLabel(JLabel label, Ride value) {
+    protected void updateLabel(JLabel label, Template value) {
         if ( value != null ){
             label.setText(value.getName());
         }
