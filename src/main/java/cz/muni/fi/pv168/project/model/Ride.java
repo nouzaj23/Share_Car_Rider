3da6cb8931c23e070536ba89be55fc5319845f12
Jan Nouza <xnouza@fi.muni.cz>
2023-11-04 11:57:04 +0100

    Refactor Ride class to extend AbstractRide

--- src/main/java/cz/muni/fi/pv168/project/model/Ride.java
+++ src/main/java/cz/muni/fi/pv168/project/model/Ride.java
@@ -2,89 +2,21 @@ package cz.muni.fi.pv168.project.model;
 
 import java.time.LocalDate;
 
-public class Ride {
-    private String name;
-    private int passengers;
-    private Currency currency;
-    private Category category;
-    private String from;
-    private String to;
-    private int distance;
-    private float hours;
+public class Ride extends AbstractRide{
     private LocalDate date;
     private boolean isCommitted = false;
 
     public Ride(String name, int passengers, Currency currency, Category category,
                 String from, String to, int distance) {
-        this.name = name;
-        this.passengers = passengers;
-        this.currency = currency;
-        this.category = category;
-        this.from = from;
-        this.to = to;
-        this.distance = distance;
+        super(name, passengers, currency, category, from, to, distance);
     }
 
-    public String getName() {
-        return name;
+    public Template extractTemplate() {
+        return new Template(name, passengers, currency, category, from, to, distance);
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPassengers() {
-        return passengers;
-    }
-
-    public void setPassengers(int passengers) {
-        if (passengers >= 0) {
-            this.passengers = passengers;
-        }
-    }
-
-    public Currency getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(Currency currency) {
-        this.currency = currency;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public int getDistance() {
-        return distance;
-    }
-
-    public float getHours() {
-        return hours;
-    }
-
-    public void setHours(float hours) {
-        this.hours = hours;
+    public static Ride exampleRide() {
+        return new Ride("Sluzobka", 3, Currency.CZK, null, "Doma", "Práce", 2);
     }
 
     public LocalDate getDate() {
@@ -95,10 +27,7 @@ public class Ride {
         this.date = date;
     }
 
-    public static Ride exampleRide() {
-        return new Ride("Sluzobka", 3, Currency.CZK, null, "Doma", "Práce", 2);
-    }
-
+    @Override
     public void setDistance(int distance) {
         if (distance >= 0) {
             var originalDistance = this.distance;
