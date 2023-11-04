42fa1b82a434f168fce27280eb223e78d3bfccbd
Jan Nouza <xnouza@fi.muni.cz>
2023-11-04 12:15:30 +0100

    Refactor TemplateDialog to use Template class

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/TemplateDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/TemplateDialog.java
@@ -2,12 +2,12 @@ package cz.muni.fi.pv168.project.ui.dialog;
 
 import cz.muni.fi.pv168.project.model.Category;
 import cz.muni.fi.pv168.project.model.Currency;
-import cz.muni.fi.pv168.project.model.Ride;
+import cz.muni.fi.pv168.project.model.Template;
 import cz.muni.fi.pv168.project.ui.model.ComboBoxModelAdapter;
 
 import javax.swing.*;
 
-public class TemplateDialog extends EntityDialog<Ride>{
+public class TemplateDialog extends EntityDialog<Template>{
     private final JTextField name = new JTextField();
     private final JSpinner passengers = new JSpinner(new SpinnerNumberModel(1, 1, Integer.MAX_VALUE, 1));
     private final ComboBoxModel<Currency> currencyModel = new DefaultComboBoxModel<>(Currency.values());
@@ -16,23 +16,23 @@ public class TemplateDialog extends EntityDialog<Ride>{
     private final JTextField to = new JTextField();
     private final JSpinner distance = new JSpinner(new SpinnerNumberModel(1, 1, Integer.MAX_VALUE, 1));
     private final JSpinner hours = new JSpinner(new SpinnerNumberModel(1, 0, Float.MAX_VALUE, 0.1));
-    private final Ride ride;
+    private final Template template;
 
-    public TemplateDialog(Ride ride, ListModel<Category> categoryListModel) {
-        this.ride = ride;
+    public TemplateDialog(Template template, ListModel<Category> categoryListModel) {
+        this.template = template;
         this.categoryModel = new JComboBox<>(new ComboBoxModelAdapter<>(categoryListModel));
         setValues();
         addFields();
     }
     private void setValues() {
-        name.setText(ride.getName());
-        passengers.setValue(ride.getPassengers());
-        currencyModel.setSelectedItem(ride.getCurrency());
-        categoryModel.setSelectedItem(ride.getCategory());
-        from.setText(ride.getFrom());
-        to.setText(ride.getTo());
-        distance.setValue(ride.getDistance());
-        hours.setValue(ride.getHours());
+        name.setText(template.getName());
+        passengers.setValue(template.getPassengers());
+        currencyModel.setSelectedItem(template.getCurrency());
+        categoryModel.setSelectedItem(template.getCategory());
+        from.setText(template.getFrom());
+        to.setText(template.getTo());
+        distance.setValue(template.getDistance());
+        hours.setValue(template.getHours());
     }
 
     private void addFields() {
@@ -47,16 +47,15 @@ public class TemplateDialog extends EntityDialog<Ride>{
     }
 
     @Override
-    Ride getEntity() {
-        ride.setName(name.getText());
-        ride.setPassengers(((Number) passengers.getValue()).intValue());
-        ride.setCurrency((Currency) currencyModel.getSelectedItem());
-        ride.setCategory((Category) categoryModel.getSelectedItem());
-        ride.setFrom(from.getText());
-        ride.setTo(to.getText());
-        ride.setDistance(((Number) distance.getValue()).intValue());
-        ride.setHours(((Number) hours.getValue()).floatValue());
-        ride.setDate(null);
-        return ride;
+    Template getEntity() {
+        template.setName(name.getText());
+        template.setPassengers(((Number) passengers.getValue()).intValue());
+        template.setCurrency((Currency) currencyModel.getSelectedItem());
+        template.setCategory((Category) categoryModel.getSelectedItem());
+        template.setFrom(from.getText());
+        template.setTo(to.getText());
+        template.setDistance(((Number) distance.getValue()).intValue());
+        template.setHours(((Number) hours.getValue()).floatValue());
+        return template;
     }
 }
