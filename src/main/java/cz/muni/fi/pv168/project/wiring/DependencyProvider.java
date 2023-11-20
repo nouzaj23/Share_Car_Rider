6c0ac0f89b08a79a7a638ec6d126b4a5edacf1d7
Jan Nouza <xnouza@fi.muni.cz>
2023-11-20 19:22:24 +0100

    Add CrudService getters to DependencyProvider

--- src/main/java/cz/muni/fi/pv168/project/wiring/DependencyProvider.java
+++ src/main/java/cz/muni/fi/pv168/project/wiring/DependencyProvider.java
@@ -4,6 +4,7 @@ import cz.muni.fi.pv168.project.business.model.Category;
 import cz.muni.fi.pv168.project.business.model.Ride;
 import cz.muni.fi.pv168.project.business.model.Template;
 import cz.muni.fi.pv168.project.business.repository.Repository;
+import cz.muni.fi.pv168.project.business.service.crud.CrudService;
 
 /**
  * Interface for instance wiring
@@ -13,4 +14,7 @@ public interface DependencyProvider {
     Repository<Ride> getRideRepository();
     Repository<Category> getCategoryRepository();
     Repository<Template> getTemplateRepository();
+    CrudService<Ride> getRideCrudService();
+    CrudService<Category> getCategoryCrudService();
+    CrudService<Template> getTemplateCrudService();
 }
