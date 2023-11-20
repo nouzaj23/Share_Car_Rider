116fd44fd965028ba6d66834ac8fee1d54546568
Jan Nouza <xnouza@fi.muni.cz>
2023-11-20 19:53:33 +0100

    Update CategoryModel to use CrudService

--- src/main/java/cz/muni/fi/pv168/project/ui/model/CategoryModel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/CategoryModel.java
@@ -1,25 +1,29 @@
 package cz.muni.fi.pv168.project.ui.model;
 
 import cz.muni.fi.pv168.project.business.model.Category;
+import cz.muni.fi.pv168.project.business.service.crud.CrudService;
 
 import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryModel extends AbstractTableModel implements EntityTableModel<Category> {
-    private final CategoryListModel categoryListModel;
+    private List<Category> categories;
+    private final CrudService<Category> categoryCrudService;
     private final List<Column<Category, ?>> columns = List.of(
             Column.editable("Name", String.class, Category::getName, Category::setName),
             Column.readonly("Rides", Integer.class, Category::getRides),
             Column.readonly("Distance", Integer.class, Category::getDistance)
     );
 
-    public CategoryModel(CategoryListModel categoryListModel) {
-        this.categoryListModel = categoryListModel;
+    public CategoryModel(CrudService<Category> categoryCrudService) {
+        this.categoryCrudService = categoryCrudService;
+        this.categories = categoryCrudService.findAll();
     }
 
     @Override
     public int getRowCount() {
-        return categoryListModel.getSize();
+        return categories.size();
     }
 
     @Override
@@ -32,10 +36,9 @@ public class CategoryModel extends AbstractTableModel implements EntityTableMode
         var category = getEntity(rowIndex);
         return columns.get(columnIndex).getValue((category));
     }
-
     @Override
     public Category getEntity(int rowIndex) {
-        return categoryListModel.getElementAt(rowIndex);
+        return categories.get(rowIndex);
     }
 
     @Override
@@ -58,22 +61,34 @@ public class CategoryModel extends AbstractTableModel implements EntityTableMode
         if (value != null) {
             var category = getEntity(rowIndex);
             columns.get(columnIndex).setValue(value, category);
+            updateRow(category);
         }
     }
 
     public void deleteRow(int rowIndex) {
-        categoryListModel.deleteRow(rowIndex);
+        var categoryToBeDeleted = getEntity(rowIndex);
+        categoryCrudService.deleteByGuid(categoryToBeDeleted.getGuid());
+        categories.remove(rowIndex);
         fireTableRowsDeleted(rowIndex, rowIndex);
     }
 
     public void addRow(Category category) {
-        int newRowIndex = categoryListModel.getSize();
-        categoryListModel.addRow(category);
+        categoryCrudService.create(category)
+                .intoException();
+        int newRowIndex = categories.size();
+        categories.add(category);
         fireTableRowsInserted(newRowIndex, newRowIndex);
     }
 
     public void updateRow(Category category) {
-        int rowIndex = categoryListModel.indexOf(category);
+        categoryCrudService.update(category)
+                .intoException();
+        int rowIndex = categories.indexOf(category);
         fireTableRowsUpdated(rowIndex, rowIndex);
+    }
+
+    public void refresh() {
+        this.categories = new ArrayList<>(categoryCrudService.findAll());
+        fireTableDataChanged();
     }
 }
