9bcf28b1b79f1f2a997a40483173e8a0c27f9d56
Jan Nouza <xnouza@fi.muni.cz>
2023-12-05 14:22:06 +0100

    Implement CategoryValidator

--- src/main/java/cz/muni/fi/pv168/project/business/service/validation/CategoryValidator.java
+++ src/main/java/cz/muni/fi/pv168/project/business/service/validation/CategoryValidator.java
@@ -1,12 +1,21 @@
 package cz.muni.fi.pv168.project.business.service.validation;
 
 import cz.muni.fi.pv168.project.business.model.Category;
+import cz.muni.fi.pv168.project.business.service.validation.common.StringLengthValidator;
+
+import java.util.List;
+
+import static cz.muni.fi.pv168.project.business.service.validation.Validator.extracting;
 
 public class CategoryValidator implements Validator<Category> {
 
     @Override
-    public ValidationResult validate(Category model) {
-        // TODO: Implement validation
-        return ValidationResult.success();
+    public ValidationResult validate(Category category) {
+        var validators = List.of(
+                extracting(
+                        Category::getName, new StringLengthValidator(1, 50, "Category name"))
+        );
+
+        return Validator.compose(validators).validate(category);
     }
 }
