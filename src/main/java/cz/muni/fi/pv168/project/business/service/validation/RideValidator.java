bcb3638e7380946d567d11dc864a4c3d01941b2e
Jan Nouza <xnouza@fi.muni.cz>
2023-12-05 15:42:45 +0100

    Add RideDateValidator usage

--- src/main/java/cz/muni/fi/pv168/project/business/service/validation/RideValidator.java
+++ src/main/java/cz/muni/fi/pv168/project/business/service/validation/RideValidator.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pv168.project.business.service.validation;
 
 import cz.muni.fi.pv168.project.business.model.Ride;
 
+import java.time.Clock;
 import java.util.List;
 
 import static cz.muni.fi.pv168.project.business.service.validation.Validator.extracting;
@@ -13,7 +14,8 @@ public class RideValidator implements Validator<Ride> {
         var validators = List.of(
                 // Reusing TemplateValidator to avoid code duplication
                 extracting(
-                        Ride::extractTemplate, new TemplateValidator())
+                        Ride::extractTemplate, new TemplateValidator()),
+                new RideDateValidator(Clock.systemUTC())
                 /* fuelExpenses validation skipped, because we allow
                  * any float value (friends can give us money for a ride,
                  * so we can potentially earn money) */
